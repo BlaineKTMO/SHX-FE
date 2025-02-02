e0f507f60756ace2ef6e830c735fb047d5a86101
Blaine <blaine.oania@gmail.com>
2025-02-01 23:30:39 -0500

    new

--- shx-fe/src/app/app.component.ts
+++ shx-fe/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { NgIf } from '@angular/common';
 
 
 @Component({
@@ -17,8 +17,14 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    NgIf,
   ], // Add required modules/components
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  private username: string;
+  constructor() {
+    this.username = sessionStorage.getItem('username');
+  }
+}
---
+++ shx-fe/src/app/image-display/image-display.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-display',
+  imports: [],
+  templateUrl: './image-display.component.html',
+  styleUrl: './image-display.component.scss'
+})
+export class ImageDisplayComponent {
+  constructor() {
+    console.log('ImageDisplayComponent created');
+  }
+}
